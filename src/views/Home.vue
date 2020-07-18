ccfb6f49f3ca4a8ea5c43654c8ab1d0986985c8f
Hurley <1401682479@qq.com>
2020-07-18 10:07:55 +0100

    Update addresses

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,15 +9,15 @@
         ><img class="title-font" src="./../assets/images/github.svg" />Developer</a
       >
       <span>/</span>
-      <a id="blog" href="http://hurley.fun"
+      <a id="blog" href="http://hurleyjames.github.io/"
         ><img class="title-font" src="./../assets/images/blogger.svg" />Blog</a
       >
       <span>/</span>
       <a id="portfolio" href="https://hurleyhuang.netlify.app/"
-        ><img class="title-font" src="./../assets/images/portfolio.svg" />Portfolio Site</a
+        ><img class="title-font" src="./../assets/images/portfolio.svg" />Portfolio</a
       >
       <span>/</span>
-      <a id="tech" href="https://tech-hurley.netlify.app//"
+      <a id="tech" href="https://tech-hurley.netlify.app/"
         ><img class="title-font" src="./../assets/images/tech.svg" />Tech Site</a
       >
     </div>
