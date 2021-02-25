78c92e67a0478c1a83773b3bedc25c909afb2f20
Hurley <1401682479@qq.com>
2021-02-25 15:58:15 +0800

    feat: update info of my academic

--- src/views/Academic.vue
+++ src/views/Academic.vue
@@ -13,14 +13,41 @@
 
       <h2>📒 Notes</h2>
       <ul>
-        <li><a href="#" target="_">[[ Social Proofs with Blockchain Consensus ]]</a> - 基于区块链共识的社会证明</li>
-        <li><a href="#" target="_">[[ Proof of Witness Presence ]]</a> - 基于区块链共识的存在证明</li>
         <li>
-          <a href="#" target="_">[[ A Manifesto for Future Generation Cloud Computing ]]</a> -
-          下一个十年的云计算研究方向
+          <a href="https://article.withh.life/2020/07/social-proofs-blockchain-consensus/" target="_"
+            >[[ Social Proofs with Blockchain Consensus ]]</a
+          >
+          - 基于区块链共识的社会证明
         </li>
         <li>
-          <a href="#" target="_">[[ Calculating the BELU Score for Text in Python ]]</a> - Python 计算文本的 BLEU 分数
+          <a href="https://article.withh.life/2020/02/witness-presence-blockchain-consensus/" target="_"
+            >[[ Proof of Witness Presence ]]</a
+          >
+          - 基于区块链共识的存在证明
+        </li>
+        <li>
+          <a href="https://article.withh.life/2020/04/future-generation-cloud-computing/" target="_"
+            >[[ A Manifesto for Future Generation Cloud Computing ]]</a
+          >
+          - 下一个十年的云计算研究方向
+        </li>
+        <li>
+          <a href="https://article.withh.life/2020/04/calculating-belu-for-text/" target="_"
+            >[[ Calculating the BELU Score for Text in Python ]]</a
+          >
+          - Python 计算文本的 BLEU 分数
+        </li>
+        <li>
+          <a href="https://article.withh.life/2021/02/cycling-risk-discomfort/" target="_"
+            >[[ On Cycling Risk and Discomfort ]]</a
+          >
+          - 基于数据驱动的骑行风险和舒适度评估
+        </li>
+        <li>
+          <a href="https://article.withh.life/2021/02/visualization-ehr/" target="_"
+            >[[ A visual data profiling tool for Electronic Health Records (EHRs) dataset ]]</a
+          >
+          -常规电子健康记录数据的可视化分析
         </li>
       </ul>
 
@@ -40,7 +67,20 @@
             >Proof of witness presence: Blockchain consensus for augmented democracy in smart cities</a
           >
         </li>
-        <li><a href="https://link.springer.com/content/pdf/10.1007/s00607-019-00771-y.pdf" target="_">On cycling risk and discomfort: urban safety mapping and bike route recommendations</a></li>
+        <li>
+          <a href="https://link.springer.com/content/pdf/10.1007/s00607-019-00771-y.pdf" target="_"
+            >On cycling risk and discomfort: urban safety mapping and bike route recommendations</a
+          >
+        </li>
+      </ul>
+
+      <h3>📊 Visualization</h3>
+      <ul>
+        <li>
+          <a href="https://pdfs.semanticscholar.org/0cf2/2fdf7c1d86ce2f7a1462a2b08c015289c8e1.pdf" target="_"
+            >Visualization of Medical Data Based on EHR Standards</a
+          >
+        </li>
       </ul>
     </div>
   </div>
@@ -50,7 +90,6 @@
 <style lang="css" scoped>
 a:hover {
   border-bottom: none;
-  font-weight: bold;
 }
 
 h2 {
